8f613ab600a0a76e88887d27fba3c9206e995bca
Mateusz Tocha <mati50013@wp.pl>
2017-04-13 22:02:47 +0200

    Grid

--- plot.js
+++ plot.js
@@ -3,6 +3,8 @@ var canvas=document.getElementById('plot');
 function plot(_canvas){
     this.canvas=_canvas;
     this.ctx=this.canvas.getContext('2d');
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle='white';
     this.widthOfPlot=this.canvas.parentElement.clientWidth;
     this.heightOfPlot=this.canvas.parentElement.clientHeight;
     this.DrawEmptyPlot();
@@ -12,20 +14,28 @@ function plot(_canvas){
   };
 }
 plot.prototype.DrawEmptyPlot=function(){
-  this.RenderGrid();
+  this.RenderVerticalGridLines();
+  this.RenderHorizontalGridLines();
 };
 
-plot.prototype.RenderGrid=function(){
+plot.prototype.RenderVerticalGridLines=function(){
 
-  for (i=0;i<10;i++){
-     this.ctx.lineWidth = 1+i;
-     this.ctx.strokeStyle='white';
+  for (i=50;i<=this.widthOfPlot;i+=50){
+
      this.ctx.beginPath();
-     this.ctx.moveTo(5.5+i*14,5);
-     this.ctx.lineTo(5.5+i*14,140);
+     this.ctx.moveTo(i+0.5,0+0.5);
+     this.ctx.lineTo(i+0.5,this.heightOfPlot+0.5);
      this.ctx.stroke();
    }
+};
 
+plot.prototype.RenderHorizontalGridLines=function(){
 
+  for (i=50;i<=this.heightOfPlot;i+=50){
+     this.ctx.beginPath();
+     this.ctx.moveTo(0.5,0+i);
+     this.ctx.lineTo(this.widthOfPlot,i+0.5);
+     this.ctx.stroke();
+   }
 };
 var myPlot=new plot(canvas);
