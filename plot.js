e74b110695660e4a04f4c613a6d964c90898c13a
Mateusz Tocha <mati50013@wp.pl>
2017-04-13 22:22:37 +0200

    Class changed
    "function" removed, class added

--- plot.js
+++ plot.js
@@ -1,41 +1,48 @@
 
 var canvas=document.getElementById('plot');
-function plot(_canvas){
+class plot{
+  constructor(_canvas){
     this.canvas=_canvas;
     this.ctx=this.canvas.getContext('2d');
     this.ctx.lineWidth = 1;
-    this.ctx.strokeStyle='white';
+    this.ctx.strokeStyle='silver';
     this.widthOfPlot=this.canvas.parentElement.clientWidth;
     this.heightOfPlot=this.canvas.parentElement.clientHeight;
     this.DrawEmptyPlot();
+  }
+  Draw(){}
+  DrawEmptyPlot(){
+    this.RenderVerticalGridLines();
+    this.RenderHorizontalGridLines();
+  }
 
-  this.Draw=function(){
+  RenderVerticalGridLines(){
 
-  };
+    for (var i=50;i<=this.widthOfPlot;i+=50){
+
+       this.ctx.beginPath();
+       this.ctx.moveTo(i+0.5,0+0.5);
+       this.ctx.lineTo(i+0.5,this.heightOfPlot+0.5);
+       this.ctx.stroke();
+     }
+  }
+
+  RenderHorizontalGridLines(){
+
+    for (var i=50;i<=this.heightOfPlot;i+=50){
+       this.ctx.beginPath();
+       this.ctx.moveTo(0.5,0+i);
+       this.ctx.lineTo(this.widthOfPlot,i+0.5);
+       this.ctx.stroke();
+     }
+  }
+
+
+
 }
-plot.prototype.DrawEmptyPlot=function(){
-  this.RenderVerticalGridLines();
-  this.RenderHorizontalGridLines();
-};
 
-plot.prototype.RenderVerticalGridLines=function(){
 
-  for (i=50;i<=this.widthOfPlot;i+=50){
 
-     this.ctx.beginPath();
-     this.ctx.moveTo(i+0.5,0+0.5);
-     this.ctx.lineTo(i+0.5,this.heightOfPlot+0.5);
-     this.ctx.stroke();
-   }
-};
 
-plot.prototype.RenderHorizontalGridLines=function(){
 
-  for (i=50;i<=this.heightOfPlot;i+=50){
-     this.ctx.beginPath();
-     this.ctx.moveTo(0.5,0+i);
-     this.ctx.lineTo(this.widthOfPlot,i+0.5);
-     this.ctx.stroke();
-   }
-};
 var myPlot=new plot(canvas);
