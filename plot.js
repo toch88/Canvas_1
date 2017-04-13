9c0b9194c2e49aaf16366e2ccca57e8ab9ab87e4
Mateusz Tocha <mati50013@wp.pl>
2017-04-13 20:21:25 +0200

    Test

--- plot.js
+++ plot.js
@@ -5,17 +5,15 @@ function plot(_canvas){
     this.ctx=this.canvas.getContext('2d');
     this.DrawEmptyPlot();
 
-  this.DrawEmptyPlot=function(){
-    this.ctx.beginPath();
-    this.ctx.strokeStyle='blue';
-    this.ctx.moveTo(20,20);
-    this.ctx.line(200,20);
-    this.ctx.stroke();
-  };
-
   this.Draw=function(){
 
   };
 }
-
+plot.prototype.DrawEmptyPlot=function(){
+  this.ctx.beginPath();
+  this.ctx.strokeStyle='blue';
+  this.ctx.moveTo(20,20);
+  this.ctx.line(200,20);
+  this.ctx.stroke();
+};
 var myPlot=new plot(canvas);
