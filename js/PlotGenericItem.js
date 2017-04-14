7cd481849ae035038057220cfd34378e721cfffc
Mateusz Tocha <mati50013@wp.pl>
2017-04-14 19:02:38 +0200

    Cleaning Stuff

--- js/PlotGenericItem.js
+++ js/PlotGenericItem.js
@@ -1,10 +1,33 @@
 class PlotGenericItem {
   constructor(_name, _position){
     this.position=_position;
+    this.points=[];
     this.name=_name;
+    this.pointsOfItem=[];
   }
 
-  Draw(){
+  Update(){
+    this.Rotate(this.deg);
+    this.Translate(this.position);
+  }
 
+  Translate(_move){
+    for (var i = 0; i < this.pointsOfItem.length; i++) {
+      this.points[i]=Vector.Add(_move,this.pointsOfItem[i]);
+    }
+  }
+
+  Rotate(deg){
+    for (var i = 0; i < this.pointsOfItem.length; i++) {
+        this.pointsOfItem[i].Rotate(deg);
+    }
+  }
+
+  Draw(ctx){
+    ctx.beginPath();
+    ctx.moveTo(this.points[0].real, this.points[0].img);
+    for (var i = 1; i < this.points.length; i++) {
+        ctx.lineTo(this.points[i].real, this.points[i].img);
+    }
   }
 }
--- js/Primitives.js
+++ js/Primitives.js
@@ -1,19 +1,17 @@
 class Line extends PlotGenericItem{
   constructor(_size){
     super("Linia", new Vector(100,100));
-    this.size=_size;
+    this.pointsOfItem=_size;
 
-    this.points=[this.position, new Vector(this.position.real+this.size.real, this.position.img+this.size.img)];
+    this.points=[this.position, new Vector(this.position.real+this.pointsOfItem.real, this.position.img+this.pointsOfItem.img)];
     this.Begin=this.points[0];
     this.End=this.points[1];
   }
 
-  Draw(ctx){
-    ctx.beginPath();
-    ctx.moveTo(this.points[0].real, this.points[0].img);
-    ctx.lineTo(this.points[1].real, this.points[1].img);
-    ctx.stroke();
-  }
+ Draw(ctx){
+   super.Draw(ctx);
+   ctx.stroke();
+ }
 }
 
 class Triangle extends PlotGenericItem{
@@ -21,10 +19,9 @@ class Triangle extends PlotGenericItem{
     if(_position===undefined){
       super("Linia", new Vector(0,0));
     }
-    else{      
+    else{
       super("Linia", _position);
     }
-
     if(_deg === undefined){
       this.deg = 0;
     }
@@ -33,38 +30,16 @@ class Triangle extends PlotGenericItem{
     }
 
     var h = _size;
-
     var b = h/(Math.cos(10*Math.PI/180));
     var c = b*Math.cos(80*Math.PI/180);
 
-    this.pointsOfTriangle = [new Vector(0, 0),new Vector(-c,-h), new Vector(c,-h)];
+    this.pointsOfItem = [new Vector(0, 0),new Vector(-c,-h), new Vector(c,-h)];
     this.points = [new Vector(0,0),new Vector(0,0),new Vector(0,0)];
     this.Update();
   }
 
-  Update(){
-    this.Rotate(this.deg);
-    this.Translate(this.position);
-  }
-
-  Translate(_move){
-    for (var i = 0; i < this.pointsOfTriangle.length; i++) {
-      this.points[i]=Vector.Add(_move, this.pointsOfTriangle[i]);
-    }
-  }
-
-  Rotate(deg){
-    for (var i = 0; i < this.pointsOfTriangle.length; i++) {
-        this.pointsOfTriangle[i].Rotate(deg);
-    }
-
-  }
-
   Draw(ctx){
-    ctx.beginPath();
-    ctx.moveTo(this.points[0].real,this.points[0].img);
-    ctx.lineTo(this.points[1].real, this.points[1].img);
-    ctx.lineTo(this.points[2].real, this.points[2].img);
+    super.Draw(ctx);
     ctx.fill();
   }
 }
